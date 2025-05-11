be624a6077d592d69b2b421a966efd811bf33fdd
Nik-Nik888 <sherbakov888@gmail.com>
2025-05-10 22:16:58 -0300

    Update vite.config.js to ensure correct publicDir for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
-  import { resolve } from "path";
+import { defineConfig } from "vite";
 
-  export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), "");
-
-    return {
-      root: ".", // Корень проекта
-      publicDir: "public", // Папка с index.html
-      build: {
-        outDir: "dist",
-        emptyOutDir: true,
-      },
-      resolve: {
-        alias: {
-          "/src": resolve(__dirname, "src"),
-        },
-      },
-      define: {
-        "import.meta.env.VITE_FIREBASE_API_KEY": JSON.stringify(
-          env.VITE_FIREBASE_API_KEY,
-        ),
-        "import.meta.env.VITE_FIREBASE_AUTH_DOMAIN": JSON.stringify(
-          env.VITE_FIREBASE_AUTH_DOMAIN,
-        ),
-        "import.meta.env.VITE_FIREBASE_PROJECT_ID": JSON.stringify(
-          env.VITE_FIREBASE_PROJECT_ID,
-        ),
-        "import.meta.env.VITE_FIREBASE_STORAGE_BUCKET": JSON.stringify(
-          env.VITE_FIREBASE_STORAGE_BUCKET,
-        ),
-        "import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
-          env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-        ),
-        "import.meta.env.VITE_FIREBASE_APP_ID": JSON.stringify(
-          env.VITE_FIREBASE_APP_ID,
-        ),
-        "import.meta.env.VITE_FIREBASE_MEASUREMENT_ID": JSON.stringify(
-          env.VITE_FIREBASE_MEASUREMENT_ID,
-        ),
-      },
-    };
+  export default defineConfig({
+    root: ".", // Корень проекта
+    publicDir: "public", // Папка с index.html
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+    },
   });
